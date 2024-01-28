86cbdcc2d94af2dea2209d40bc289612bbd918d3
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2024-01-28 17:43:09 +0530

    fix-pre-commit-hook
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- src/sections/UseCases/index.js
+++ src/sections/UseCases/index.js
@@ -52,7 +52,7 @@ const UseCases = () => {
       <Container>
         <div className="workshop-grid-wrapper">
           <Row>
-            {data.allMdx.nodes.map(({ id, frontmatter}) => (
+            {data.allMdx.nodes.map(({ id, frontmatter }) => (
               <Col {...content && ID === id ? { xs: 12, sm: 12, lg: 12 } : { xs: 12, sm: 6, lg: 4 } } key={id} className="workshop-grid-col">
                 <div className="workshop-grid-card">
                   <WorkshopCard frontmatter={frontmatter} content={content} ID={ID} id={id} />
